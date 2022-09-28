88b13a3db1c7f8001ec50f2ef0ea1d2a389b40ef
noname <48761044+noname0310@users.noreply.github.com>
2022-09-28 20:38:06 +0900

    feat: module isolation (#36)
    * feat: add treeshaker transformer
    
    * build: add deque isolate build
    
    * fix: resolve merge
    
    * style: create isolate module build pipeline
    
    * build: stack container build, isolated cjs build
    
    * build: create isolate tasks for all containers
    
    * fix: missing caret
    
    * build: change isolate cjs build target to ES6
    
    * build: clean up build scripts
    
    * fix: fix compile error for build script
    
    * chore: remove comment
    
    * build: update yarn.lock
    
    * refactor: refactoring build script
    
    * refactor: refactoring build script
    
    * build: add isolate package files auto gen
    
    * build: add index import support
    
    * fix: resolve merge
    
    * build: index import solve
    
    * build: fix failing ci
    
    * build: add version option to isolate build
    
    * feat: create auto publish script
    
    * fix: resolve merge
    
    * fix: add missing package
    
    * fix: add missing packages
    
    * refactor: apply request changes
    
    * refactor: apply request changes
    
    * build: use isolate.config for publish
    
    * style: fix typo
    
    * refactor: apply request changes

---
+++ tools/buildFactory.ts
@@ -0,0 +1,235 @@
+import gulp from 'gulp';
+import tap from 'gulp-tap';
+import filter from 'gulp-filter';
+import merge from 'merge-stream';
+import ts from 'gulp-typescript';
+import terser from 'gulp-terser';
+import GulpUglify from 'gulp-uglify';
+import babel from 'gulp-babel';
+import rename from 'gulp-rename';
+import { SrcOptions } from 'vinyl-fs';
+import sourcemaps from 'gulp-sourcemaps';
+import tsMacroTransformer, { macros } from 'ts-macros';
+import pathsTransformer from 'ts-transform-paths';
+import rollupTypescript from 'rollup-plugin-typescript2';
+import { babel as rollupBabel } from '@rollup/plugin-babel';
+import { CustomTransformerFactory, Program } from 'typescript';
+import tsTreeshaker, { DependencySolver } from './tsTreeshaker';
+import deleteEmpty from 'delete-empty';
+import ttypescript from 'ttypescript';
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const clean = require('gulp-clean') as () => NodeJS.ReadWriteStream;
+const rollup = require('gulp-rollup-2').rollup as (...params: unknown[]) => NodeJS.ReadWriteStream;
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+function createProject(overrideSettings?: ts.Settings) {
+  return ts.createProject('tsconfig.json', {
+    ...overrideSettings,
+    getCustomTransformers: (program?: Program) => {
+      if (program === undefined) throw new Error('Program is undefined');
+
+      const pathsTransform = pathsTransformer(program);
+
+      const overrideGetCustomTransformers = overrideSettings?.getCustomTransformers;
+      if (typeof (overrideGetCustomTransformers) === 'string') {
+        throw new Error('getCustomTransformers should be a function');
+      }
+      const customTransformers = overrideGetCustomTransformers?.(program);
+
+      return {
+        before: [
+          tsMacroTransformer(program) as unknown as CustomTransformerFactory,
+          ...customTransformers?.before ?? []
+        ],
+        after: [
+          ...pathsTransform.after,
+          ...customTransformers?.after ?? []
+        ],
+        afterDeclarations: [
+          ...pathsTransform.afterDeclarations,
+          ...customTransformers?.afterDeclarations ?? []
+        ]
+      };
+    }
+  });
+}
+
+function terserStream() {
+  return terser({
+    compress: false,
+    output: {
+      beautify: true
+    },
+    mangle: {
+      keep_fnames: true,
+      keep_classnames: true,
+      properties: {
+        regex: /^_/
+      }
+    },
+    nameCache: {}
+  });
+}
+
+function rollupStream(input: string) {
+  return rollup({
+    input,
+    output: {
+      file: input,
+      format: 'umd',
+      name: 'sdsl'
+    },
+    context: 'this',
+    plugins: [
+      rollupTypescript({
+        typescript: ttypescript,
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'ES5',
+            module: 'ES2015',
+            declaration: true
+          }
+        }
+      }),
+      rollupBabel({
+        babelHelpers: 'bundled',
+        extensions: ['.ts', '.js'],
+        plugins: ['babel-plugin-remove-unused-import']
+      })
+    ]
+  });
+}
+
+function babelStream(removeUnusedImport: boolean, cjsTransform: boolean) {
+  return (removeUnusedImport || cjsTransform)
+    ? babel({
+      plugins: [
+        removeUnusedImport ? 'babel-plugin-remove-unused-import' : undefined,
+        cjsTransform ? '@babel/plugin-transform-modules-commonjs' : undefined
+      ].filter((plugin) => plugin !== undefined) as string[]
+    })
+    : tap(() => { /* */ });
+}
+
+export function gulpFactory(
+  input: {
+    globs: string | string[],
+    opts?: SrcOptions
+  },
+  output: string,
+  overrideSettings?: Omit<ts.Settings, 'outDir'>,
+  useCjsTransform = false
+): NodeJS.ReadWriteStream {
+  macros.clear();
+  const tsProject = createProject({
+    ...overrideSettings,
+    outDir: output
+  });
+  const cleanStream = gulp.src(output, { read: false, allowEmpty: true })
+    .pipe(clean());
+  const tsBuildResult = gulp.src(input.globs, input.opts)
+    .pipe(tsProject());
+  const jsBuildResult = tsBuildResult.js
+    .pipe(babelStream(true, useCjsTransform))
+    .pipe(terserStream());
+  return merge(
+    cleanStream,
+    merge([tsBuildResult.dts, jsBuildResult])
+      .pipe(filter(['**/*.ts', '**/*.js', '!**/*.macro.d.ts', '!**/*.macro.js']))
+      .pipe(gulp.dest(output)));
+}
+
+export function gulpUmdFactory(input: string, output: string) {
+  macros.clear();
+  return gulp
+    .src([`dist/umd/${output}`], { read: false, allowEmpty: true })
+    .pipe(clean())
+    .pipe(gulp.src(input))
+    .pipe(rollupStream(input))
+    .pipe(terserStream())
+    .pipe(rename(output))
+    .pipe(gulp.dest('dist/umd'));
+}
+
+export function gulpUmdMinFactory(input: string, output: string) {
+  macros.clear();
+  return gulp
+    .src([`dist/umd/${output}`, `dist/umd/${output}.map`], { read: false, allowEmpty: true })
+    .pipe(clean())
+    .pipe(gulp.src(input))
+    .pipe(sourcemaps.init())
+    .pipe(GulpUglify({ compress: true }))
+    .pipe(rename(output))
+    .pipe(sourcemaps.write('.', { includeContent: false }))
+    .pipe(gulp.dest('dist/umd'));
+}
+
+export function gulpIsolateFactory(
+  taskPrefix: string,
+  input: {
+    sourceRoots: string | string[],
+    indexFile: string,
+    globs: string | string[],
+    opts?: SrcOptions
+  },
+  output: string,
+  overrideSettings?: Omit<ts.Settings, 'outDir'>,
+  useCjsTransform = false
+) {
+  const dependencySolver = new DependencySolver(
+    input.sourceRoots,
+    {
+      baseUrl: input.opts?.base ?? '.',
+      outDir: output
+    }
+  );
+
+  function build() {
+    return gulpFactory(
+      input,
+      output,
+      {
+        ...overrideSettings,
+        getCustomTransformers: (program?: Program) => {
+          const treeShakerTransform = tsTreeshaker({
+            solver: dependencySolver,
+            indexFile: input.indexFile
+          });
+
+          const customTransformers = overrideSettings?.getCustomTransformers?.(program);
+          return {
+            before: customTransformers?.before,
+            after: [
+              ...treeShakerTransform.after,
+              ...customTransformers?.after ?? []
+            ],
+            afterDeclarations: [
+              ...treeShakerTransform.afterDeclarations,
+              ...customTransformers?.afterDeclarations ?? []
+            ]
+          };
+        }
+      },
+      useCjsTransform
+    );
+  }
+  build.displayName = `${taskPrefix}-build`;
+
+  function filterDependencies() {
+    return gulp.src(`${output}/**/*`, { read: false, allowEmpty: true, base: '.' })
+      .pipe(filter([
+        '**/*.ts',
+        '**/*.js',
+        ...dependencySolver.getIncludedDependencies().map((dep) => `!${dep}`)]))
+      .pipe(clean());
+  }
+  filterDependencies.displayName = `${taskPrefix}-filter-dependencies`;
+
+  async function cleanEmptyDirs() {
+    await deleteEmpty(process.cwd() + '/' + output);
+  }
+  cleanEmptyDirs.displayName = `${taskPrefix}-clean-empty-dirs`;
+
+  return gulp.series(build, filterDependencies, cleanEmptyDirs);
+}
