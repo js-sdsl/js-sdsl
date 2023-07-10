ebc57a60e91db4f4efd87f868cf6a8c40a563def
ZLY201 <951711127@qq.com>
2023-07-10 13:35:19 +0800

    perf: extract the function of tree node to the outside

--- src/tree/tree-node.ts
+++ src/tree/tree-node.ts
@@ -38,96 +38,103 @@ type TreeNodeProps<K, V> = {
   value?: V;
 };
 
+function prev<K, V>(this: TreeNode<K, V>) {
+  let prevNode = this;
+  const isRootOrHeader = prevNode._parent!._parent === prevNode;
+  if (isRootOrHeader && prevNode._color === TREE_NODE_COLOR.RED) {
+    prevNode = prevNode._right!;
+  } else if (prevNode._left) {
+    prevNode = prevNode._left;
+    while (prevNode._right) {
+      prevNode = prevNode._right;
+    }
+  } else {
+    // Must be root and left is null
+    if (isRootOrHeader) {
+      return prevNode._parent!;
+    }
+    let prev = prevNode._parent!;
+    while (prev._left === prevNode) {
+      prevNode = prev;
+      prev = prevNode._parent!;
+    }
+    prevNode = prev;
+  }
+  return prevNode;
+}
+
+function next<K, V>(this: TreeNode<K, V>) {
+  let nextNode = this;
+  if (nextNode._right) {
+    nextNode = nextNode._right;
+    while (nextNode._left) {
+      nextNode = nextNode._left;
+    }
+    return nextNode;
+  } else {
+    let prev = nextNode._parent!;
+    while (prev._right === nextNode) {
+      nextNode = prev;
+      prev = nextNode._parent!;
+    }
+    if (nextNode._right !== prev) {
+      return prev;
+    } else return nextNode;
+  }
+}
+
+function rotateLeft<K, V>(this: TreeNode<K, V>) {
+  const PP = this._parent!;
+  const T = this._right!;
+  const R = T._left;
+
+  if (PP._parent === this) PP._parent = T;
+  else if (PP._left === this) PP._left = T;
+  else PP._right = T;
+
+  T._parent = PP;
+  T._left = this;
+
+  this._parent = T;
+  this._right = R;
+
+  if (R) R._parent = this;
+
+  return T;
+}
+
+function rotateRight<K, V>(this: TreeNode<K, V>) {
+  const PP = this._parent!;
+  const F = this._left!;
+  const T = F._right;
+
+  if (PP._parent === this) PP._parent = F;
+  else if (PP._left === this) PP._left = F;
+  else PP._right = F;
+
+  F._parent = PP;
+  F._right = this;
+
+  this._parent = F;
+  this._left = T;
+
+  if (T) T._parent = this;
+
+  return F;
+}
+
 export function createTreeNode<K, V>(props: TreeNodeProps<K, V> = {}): TreeNode<K, V> {
-  const { color = TREE_NODE_COLOR.RED, key, value } = props;
   return {
-    _key: key,
-    _value: value,
-    _color: color,
+    _key: props.key,
+    _value: props.value,
+    _color: props.color === undefined ? TREE_NODE_COLOR.RED : props.color,
     _left: undefined,
     _right: undefined,
     _parent: undefined,
-    _prev() {
-      let prevNode: TreeNode<K, V> = this;
-      const isRootOrHeader = prevNode._parent!._parent === prevNode;
-      if (isRootOrHeader && prevNode._color === TREE_NODE_COLOR.RED) {
-        prevNode = prevNode._right!;
-      } else if (prevNode._left) {
-        prevNode = prevNode._left;
-        while (prevNode._right) {
-          prevNode = prevNode._right;
-        }
-      } else {
-        // Must be root and left is null
-        if (isRootOrHeader) {
-          return prevNode._parent!;
-        }
-        let prev = prevNode._parent!;
-        while (prev._left === prevNode) {
-          prevNode = prev;
-          prev = prevNode._parent!;
-        }
-        prevNode = prev;
-      }
-      return prevNode;
-    },
-    _next() {
-      let nextNode: TreeNode<K, V> = this;
-      if (nextNode._right) {
-        nextNode = nextNode._right;
-        while (nextNode._left) {
-          nextNode = nextNode._left;
-        }
-        return nextNode;
-      } else {
-        let prev = nextNode._parent!;
-        while (prev._right === nextNode) {
-          nextNode = prev;
-          prev = nextNode._parent!;
-        }
-        if (nextNode._right !== prev) {
-          return prev;
-        } else return nextNode;
-      }
-    },
-    _rotateLeft() {
-      const PP = this._parent!;
-      const V = this._right!;
-      const R = V._left;
-
-      if (PP._parent === this) PP._parent = V;
-      else if (PP._left === this) PP._left = V;
-      else PP._right = V;
-
-      V._parent = PP;
-      V._left = this;
-
-      this._parent = V;
-      this._right = R;
-
-      if (R) R._parent = this;
-
-      return V;
-    },
-    _rotateRight() {
-      const PP = this._parent!;
-      const F = this._left!;
-      const K = F._right;
-
-      if (PP._parent === this) PP._parent = F;
-      else if (PP._left === this) PP._left = F;
-      else PP._right = F;
-
-      F._parent = PP;
-      F._right = this;
-
-      this._parent = F;
-      this._left = K;
-
-      if (K) K._parent = this;
-
-      return F;
-    }
+    _prev: prev,
+    _next: next,
+    _rotateLeft: rotateLeft,
+    _rotateRight: rotateRight
   };
 }
 
@@ -138,51 +145,50 @@ export type TreeNodeEnableIndex<K, V> = {
   _subTreeSize: number;
   _prev(): TreeNodeEnableIndex<K, V>;
   _next(): TreeNodeEnableIndex<K, V>;
-  __rotateLeft(): TreeNodeEnableIndex<K, V>;
-  __rotateRight(): TreeNodeEnableIndex<K, V>;
   _rotateLeft(): TreeNodeEnableIndex<K, V>;
   _rotateRight(): TreeNodeEnableIndex<K, V>;
   _recount(): void;
 } & TreeNode<K, V>;
 
+function rotateLeftEnableIndex<K, V>(this: TreeNodeEnableIndex<K, V>) {
+  const parent = rotateLeft.apply(this) as TreeNodeEnableIndex<K, V>;
+  this._recount();
+  parent._recount();
+  return parent;
+}
+
+function rotateRightEnableIndex<K, V>(this: TreeNodeEnableIndex<K, V>) {
+  const parent = rotateRight.apply(this) as TreeNodeEnableIndex<K, V>;
+  this._recount();
+  parent._recount();
+  return parent;
+}
+
+function recount<K, V>(this: TreeNodeEnableIndex<K, V>) {
+  this._subTreeSize = 1;
+  if (this._left) {
+    this._subTreeSize += this._left._subTreeSize;
+  }
+  if (this._right) {
+    this._subTreeSize += this._right._subTreeSize;
+  }
+}
+
 export function createTreeNodeEnableIndex<K, V>(
   props: TreeNodeProps<K, V> = {}
 ): TreeNodeEnableIndex<K, V> {
-  const {
-    _color, _key, _value, _prev, _next, _rotateLeft, _rotateRight
-  } = createTreeNode(props) as TreeNodeEnableIndex<K, V>;
   return {
-    _key,
-    _value,
-    _color,
+    _key: props.key,
+    _value: props.value,
+    _color: props.color === undefined ? TREE_NODE_COLOR.RED : props.color,
     _left: undefined,
     _right: undefined,
     _parent: undefined,
-    _prev,
-    _next,
+    _prev: prev as (() => TreeNodeEnableIndex<K, V>),
+    _next: next as (() => TreeNodeEnableIndex<K, V>),
     _subTreeSize: 1,
-    __rotateLeft: _rotateLeft,
-    __rotateRight: _rotateRight,
-    _rotateLeft() {
-      const parent = this.__rotateLeft();
-      this._recount();
-      parent._recount();
-      return parent;
-    },
-    _rotateRight() {
-      const parent = this.__rotateRight();
-      this._recount();
-      parent._recount();
-      return parent;
-    },
-    _recount() {
-      this._subTreeSize = 1;
-      if (this._left) {
-        this._subTreeSize += this._left._subTreeSize;
-      }
-      if (this._right) {
-        this._subTreeSize += this._right._subTreeSize;
-      }
-    }
+    _rotateLeft: rotateLeftEnableIndex,
+    _rotateRight: rotateRightEnableIndex,
+    _recount: recount
   };
 }
